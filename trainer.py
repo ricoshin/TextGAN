5e540bcc378a2d892efe3b6165c57c852578c59f
Wuhyun Shin <rico.shin@gmail.com>
2017-05-23 20:39:43 +0900

    Update

--- trainer.py
+++ trainer.py
@@ -9,22 +9,33 @@ import tensorflow as tf
 from models import *
 
 class Trainer(object):
-    def __init__(self, config):
+    def __init__(self, config, data, W_e_init):
         #  self, W_e_init, max_sentence_len, num_classes, vocab_size,
         #  embedding_size, filter_sizes, num_filters, data_format, l2_reg_lambda=0.0
-        self.data = FROM_HW
-        self.W_e_init = FROM_HW
-        self.max_sentence_len = FROM_HW
-        self.vocab_size = FROM_HW
-        self.embedding_size = FROM_HW
+        #MAX_SENTENCE_LEN = 20
+        #VOCAB_SIZE = 1000
+        #EMBEDDING_SIZE = 300
+        self.batch_size = config.batch_size
+        #self.data = np.random.randint(VOCAB_SIZE, size=(self.batch_size, MAX_SENTENCE_LEN))
+        #self.W_e_init = np.random.rand(VOCAB_SIZE, EMBEDDING_SIZE)
+        self.max_sentence_len = data.shape[1]
+        self.vocab_size = W_e_init.shape[0]
+        self.embedding_size = W_e_init.shape[1]
 
-        self.filter_sizes = config.filter_sizes
-        self.num_filters = config.num_filters
+        def convert_to_onehot(labels):
+            num_classes = np.max(labels)+1
+            return np.eye(num_classes)[labels]
+
+        self.y_real = convert_to_onehot(np.ones(self.batch_size, dtype=np.int))
+        self.y_fake = convert_to_onehot(np.zeros(self.batch_size, dtype=np.int))
+
+        self.filter_sizes = [3,4,5]
+        self.num_filters = 300
+        self.dropout_keep_prob = 0.5
+        self.l2_reg_lambda = 0.0
         self.data_format = config.data_format
-        self.droput_prob = config.droput_prob
 
         self.optimizer = config.optimizer
-        self.batch_size = config.batch_size
         self.z_num = config.z_num
 
         self.model_dir = config.model_dir # e.g. logs/MyDataSet_xxx
@@ -33,6 +44,7 @@ class Trainer(object):
         self.use_gpu = config.use_gpu
         self.g_lr, self.d_lr = config.g_lr, config.d_lr
 
+        self.global_step = tf.Variable(0, name="global_step", trainable=False)
         self.start_step = 0
         self.log_step = config.log_step
         self.max_step = config.max_step
@@ -41,8 +53,9 @@ class Trainer(object):
 
         self.is_train = config.is_train
 
+
+
         self.build_model()
-        self.plan_optimize()
 
         self.saver = tf.train.Saver()
         self.summary_writer = tf.summary.FileWriter(self.model_dir)
@@ -52,7 +65,7 @@ class Trainer(object):
                                 summary_op=None,
                                 summary_writer=self.summary_writer,
                                 save_model_secs=300, # 5 minutes
-                                global_step=self.myGAN.step,
+                                global_step=self.global_step,
                                 ready_for_local_init_op=None)
 
         gpu_options = tf.GPUOptions(allow_growth=True)
@@ -64,78 +77,92 @@ class Trainer(object):
 
     def build_model(self):
 
+
         #  self, W_e_init, max_sentence_len, num_classes, vocab_size,
         #  embedding_size, filter_sizes, num_filters, data_format, l2_reg_lambda=0.0
-        discriminator = Discriminator(self.W_e_init, self.max_sentence_len, 2,
-                                        self.vocab_size, self.embedding_size, self.num_filters,
-                                        self.num_filters, self.data_format, self.l2_reg_lambda)
+        self.D = Discriminator(W_e_init=self.W_e_init,
+                                max_sentence_len=self.max_sentence_len,
+                                num_classes=2,
+                                vocab_size=self.vocab_size,
+                                embedding_size=self.embedding_size,
+                                filter_sizes=self.filter_sizes,
+                                num_filters=self.num_filters,
+                                data_format=self.data_format,
+                                l2_reg_lambda=self.l2_reg_lambda)
 
-        if optimizer == 'adam':
+        if self.optimizer == 'adam':
             self.optimizer = tf.train.AdamOptimizer
         else:
-            raise Exception("[!] Caution! Paper didn't use {} opimizer other than Adam".format(config.optimizer))
+            raise Exception("[!] Not supported optimizer")
 
-        self.g_lr = tf.Variable(g_lr, name='g_lr')
-        self.d_lr = tf.Variable(d_lr, name='d_lr')
-
-        # What the heck?
-        self.g_lr_update = tf.assign(self.g_lr, (self.g_lr * 0.5), name='g_lr_update')
+        #self.g_lr = tf.Variable(g_lr, name='g_lr')
+        self.d_lr = tf.Variable(self.d_lr, name='d_lr')
+        #self.g_lr_update = tf.assign(self.g_lr, (self.g_lr * 0.5), name='g_lr_update')
         self.d_lr_update = tf.assign(self.d_lr, (self.d_lr * 0.5), name='d_lr_update')
+        #g_optimizer = self.optimizer(self.g_lr)
+        d_optimizer = self.optimizer(self.d_lr)
 
-        #import pdb; pdb.set_trace()
-        g_optimizer, d_optimizer = self.optimizer(self.g_lr), self.optimizer(self.d_lr)
-
-
-        d_optim = d_optimizer.minimize(self.TOTAL_D_loss, var_list=self.D_var)
-        g_optim = g_optimizer.minimize(self.TOTAL_G_loss, global_step=self.step, var_list=self.G_var)
+        #g_optim = g_optimizer.minimize(self.G_loss, global_step=self.step, var_list=self.G_var)
+        grads_and_vars = d_optimizer.compute_gradients(self.D.loss)
+        self.d_train_op = d_optimizer.apply_gradients(grads_and_vars, global_step=self.global_step)
         # global_step: Optional Variable to increment by one after the variables have been updated.
 
-        # Context manager : k_update will be only assigned after d_optim and g_optim have been computed.
-        with tf.control_dependencies([d_optim, g_optim]):
-            self.k_update = tf.assign(self.k_t, tf.clip_by_value(self.k_t + lambda_k * self.balance, 0, 1))
+        # Keep track of gradient values and sparsity (optional)
+        grad_summaries = []
+        for _grads, _vars in grads_and_vars:
+            if _grads is not None:
+                grad_hist_summary = tf.summary.histogram("{}/grad/hist".format(_vars.name), _grads)
+                sparsity_summary = tf.summary.scalar("{}/grad/sparsity".format(_vars.name), tf.nn.zero_fraction(_grads))
+                grad_summaries.append(grad_hist_summary)
+                grad_summaries.append(sparsity_summary)
+        grad_summaries_merged = tf.summary.merge(grad_summaries)
 
         self.summary_op = tf.summary.merge([
-
-            tf.summary.scalar("misc/k_t", self.k_t),
-            tf.summary.scalar("misc/d_lr", self.d_lr),
-            tf.summary.scalar("misc/g_lr", self.g_lr),
-
+            tf.summary.scalar("D_loss", self.D.loss),
+            tf.summary.scalar("D_accuracy", self.D.accuracy),
+            tf.summary.scalar("D_learning_rate", self.d_lr),
+            grad_summaries_merged
         ])
 
-        self.g_lr_update = tf.assign(self.g_lr, (self.g_lr * 0.5), name='g_lr_update')
         self.d_lr_update = tf.assign(self.d_lr, (self.d_lr * 0.5), name='d_lr_update')
 
     def train(self):
 
         for step in trange(self.start_step, self.max_step): # 0 - 500000
-            fetch_dict = { # evaluate every step
 
+            fetch_dict = {
+                "d_train_op": self.d_train_op,
+                "global_step": self.global_step,
+                "D_loss": self.D.loss,
+                "D_accuracy": self.D.accuracy
+            }
+            feed_dict = { # evaluate every step
+                self.D.input_x: self.data,
+                self.D.input_y: self.y_real,
+                self.D.dropout_keep_prob: self.dropout_keep_prob
             }
             if step % self.log_step == 0: # default : 50
                 fetch_dict.update({
-
+                    "summary": self.summary_op
                 })
-            result = self.sess.run(fetch_dict)
-
-            measure = result['measure']
-            measure_history.append(measure)
+            result = self.sess.run(fetch_dict, feed_dict)
 
             if step % self.log_step == 0: # default : 50
                 self.summary_writer.add_summary(result['summary'], step)
                 self.summary_writer.flush()
 
-                g_loss = result['TOTAL_G_loss']
-                d_loss = result['TOTAL_D_loss']
-                k_t = result['k_t']
+                d_loss = result['D_loss']
+                d_accuracy = result['D_accuracy']
 
-                print("[{}/{}] Loss_D: {:.6f} Loss_G: {:.6f} measure: {:.4f}, k_t: {:.4f}". \
-                      format(step, self.max_step, d_loss, g_loss, measure, k_t))
+                print("[{}/{}] D_loss: {:.6f} D_accuracy: {:.6f} ". \
+                      format(step, self.max_step, d_loss, d_accuracy))
 
             if step % (self.log_step * 10) == 0: # default : every 100 steps
                 pass
 
             if step % self.lr_update_step == self.lr_update_step - 1:
-                pass
+                self.sess.run(self.d_lr_update)
 
     def test(self):
+
         pass
